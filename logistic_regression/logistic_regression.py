Kinda fixed logregression, need to add metrics calculation

logistic_regression/logistic_regression.py
@@ -20,21 +20,30 @@ class MyLogisticRegression:
         if self.w is None:
             self.w = np.random.randn(k + 1)
         
-        X_train = np.concatenate((np.ones((n, 1)), X), axis=1)
-        
+        X_train = np.concatenate((X, np.ones((n, 1))), axis=1)
+        losses = []
         for iter_num in range(max_iter):
             z = sigmoid(logit(X_train, self.w))
-            grad = np.dot(X_train.T, (z - y)) / len(y)
+            grad = np.dot(X_train.T, (z - y)) / n
             self.w -= grad * lr
+            losses.append(-self._loss(y, z))
+
+        return losses
 
     def predict_proba(self, X):
         n, k = X.shape
-        X_ = np.concatenate((np.ones((n, 1)), X), axis=1)
+        X_ = np.concatenate((X, np.ones((n, 1))), axis=1)
         return sigmoid(logit(X_, self.w))
 
-    def score(self, y_true, p):
-        return self._loss(y_true, p)
-        # TODO: calculate all metrics (accuracy, TPR, precision, F1)
+    def score(self, y_true, pred_probs):
+        return self._loss(y_true, pred_probs)
+
+    def get_metrics(self, y_true, y_pred):
+        metrics = {}
+        diff = y_pred - y_true
+        metrics['Accuracy'] = 1.0 - (float(np.count_nonzero(diff)) / len(diff))
+
+        return metrics
 
     def predict(self, X, threshold=0.5):
         return self.predict_proba(X) >= threshold